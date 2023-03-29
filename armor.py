949f70c7354320528941b7f10bdb697e69620971
aalberty <46965141+aalberty@users.noreply.github.com>
2023-03-29 19:41:25 +0000

    levelset

--- armor.py
+++ armor.py
@@ -1,17 +1,39 @@
 class Armor:
-    def __init__(self, name, tier, item_type, power, power_limit, stats):
+    def __init__(self, name, tier, item_type, equipable, power, power_limit, stats):
         self.name = name
         self.tier = tier
         self.type = item_type
+        self.equipable = equipable
         self.power = power
         self.power_limit = power_limit
         self.stats = stats
+
+    def __str__(self):
+        return self.name
     
     @classmethod
     def fromCSVFile(cls, filename):
         return loadCSV(filename)
 
+    @classmethod
+    def getHunterItems(cls, items):
+        return getClassItems(items, 'Hunter')
+
+    @classmethod
+    def getWarlockItems(cls, items):
+        return getClassItems(items, 'Warlock')
+
+    @classmethod
+    def getTitanItems(cls, items):
+        return getClassItems(items, 'Titan')  
         
+
+def getClassItems(items, classname):
+    filteredItems = []
+    for item in items:
+        if item.equipable == classname:
+            filteredItems.append(item)
+    return filteredItems
 
 def loadCSV(filename):
     file = open(filename, 'r')
@@ -33,6 +55,7 @@ def loadCSV(filename):
             name=raw_item[0],
             tier=raw_item[4],
             item_type=raw_item[5],
+            equipable=raw_item[7],
             power=raw_item[8],
             power_limit=raw_item[9],
             stats=stats
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 import armor
-items = armor.fromCSVFile('destinyArmor.csv')
+items = armor.Armor.fromCSVFile('destinyArmor.csv')
 # Sort by type
+types = ["Helmet", "Gauntlets", "Chest Armor", "Leg Armor"]
+class_item_types = {
+    "Hunter": "Hunter Cloak",
+    "Warlock": "Warlock Bond",
+    "Titan": "Titan Mark"
+}
+
+def loadClassArmor(guardian_class):
+    class_item_type = class_item_types[guardian_class]
+    class_armor = list(filter(lambda x: x.equipable == guardian_class, items))
+
+    helmets = filter(lambda x: x.type == types[0], class_armor)
+    gauntlets = filter(lambda x: x.type == types[1], class_armor)
+    chest = filter(lambda x: x.type == types[2], class_armor)
+    leg = filter(lambda x: x.type == types[3], class_armor)
+    citem = filter(lambda x: x.type == class_item_type, class_armor)
+    return {
+        "helmets": list(helmets), 
+        "gauntlets": list(gauntlets), 
+        "chest": list(chest), 
+        "leg": list(leg), 
+        "citem": list(citem)
+        }
